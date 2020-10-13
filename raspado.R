7e28d157c066f9b5eb9b1a99a9e702fbcbbb3f3d
J. Kniffki <26249992+kifirifis@users.noreply.github.com>
2020-10-13 16:53:29 +0200

    primer commit

---
+++ raspado.R
@@ -0,0 +1,62 @@
+                                #RASPADO
+# Este script realiza un raspado de la página https://coronavirus.asturias.es/ que es el
+# Portal de Transparencia en Datos de Asturias, crea un csv con el nombre 
+# asturias.csv. Se ejecuta con tarea programada todos los días salvo los fines de semana y fiestas de guardar (jua jua)
+# Se imputa el día actual al anterior y se calculan los casos nuevos. 
+
+if (!("rvest" %in% installed.packages())) {
+        install.packages("rvest")
+}
+if (!("tidyverse" %in% installed.packages())) {
+        install.packages("tidyverse")
+}
+
+library(rvest)
+library(tidyverse)
+
+#==============  Raspado =========== 
+pagina <-  "https://coronavirus.asturias.es/"
+html <- read_html(pagina)
+
+html %>% 
+        html_nodes(xpath = '//*[@class="numero"]') %>% 
+        html_text()  -> value
+
+value <- as.numeric(gsub("[.]", "", value)) #quita separador de miles
+
+html %>% 
+        html_nodes(xpath = '//*[@class="titulo"]') %>% 
+        html_text() -> key
+
+#=============== data.frame ========
+df <- data.frame(key = key, value = value) %>% 
+        spread(key, value) %>% 
+        mutate(date = Sys.Date() - 1, # Los datos de hoy son los de ayer 
+               province = "Asturias",
+               ccaa = "Asturias, Principado de") %>% 
+        select(-`Casos confirmados en personal sanitario`)
+
+#nombres de las variables
+names(df) <- c("cases_accumulated_pcr", 
+               "recovered",
+               "deceased",
+               "hospitalized",
+               "intensive_care",
+               "muestras.pcr",
+               "muestras.testac",
+               "date", "province", "ccaa")
+
+#Datos del drive
+datos <- read.csv("data/asturias.csv", header = T)
+names(datos) <- tolower(names(datos))
+datos$date <- lubridate::as_date(datos$date)
+datos <- datos %>% select(names(df))
+
+datos <- full_join(datos, df) 
+
+#TO-DO Solo tomar el último lag no NA RESOLVER
+datos <- datos %>% mutate(new_cases = lag(cases_accumulated_pcr - lag(cases_accumulated_pcr)))
+
+write.csv(datos, "data/procesadoasturias.csv", row.names = F)
+
+#TO-DO Poner el git aquí con el cron (después del anterior TO-DO)
