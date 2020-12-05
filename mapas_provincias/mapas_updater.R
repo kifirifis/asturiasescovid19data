ca1432093f10d4d14bbfa6eaf02919ac188b8cef
J. Kniffki <26249992+kifirifis@users.noreply.github.com>
2020-12-05 18:13:17 +0100

    update mapas

--- mapas_provincias/mapas_updater.R
+++ mapas_provincias/mapas_updater.R
@@ -42,8 +42,8 @@ shp_df <- broom::tidy(shp)
 # Unir por id
 shp_df <- dplyr::left_join(shp_df, shp@data, by = "id")
 
-# Leer datos escovid19
-df <- read.csv("https://raw.githubusercontent.com/montera34/escovid19data/master/data/output/covid19-provincias-spain_consolidated.csv", header = T, encoding = "UTF-8")  
+# Leer datos scovid19data
+df <- read.csv("https://raw.githubusercontent.com/montera34/escovid19data/master/data/output/covid19-provincias-spain_consolidated.csv", header = T, encoding = "UTF-8")  #TODO Probar cargar con fread
 
 #======= Preparación de los datos ======== 
 
@@ -52,12 +52,12 @@ fecha <- Sys.Date() - 4 # 3 días antes que suelen estar completos los datos
 
 # Último día completo y var ia14
 datos <- df %>% # TODO Renombrar variables para hacer el bucle
-        dplyr::select(date, province, ine_code, ia14) %>% 
+        dplyr::select(date, province, ine_code, ia14, deceassed_per_100000) %>% 
         dplyr::filter(date == fecha)
 
 shp_df$ine_code <- as.numeric(shp_df$ine_code) # Numérico como en los datos
 
-#Unión 
+#Unión shp_df-datos
 
 ifelse( 
         dim(dplyr::anti_join(datos, shp_df))[1] == 0,
@@ -70,12 +70,12 @@ cerca <- unido %>%
         dplyr::mutate(lat = ifelse(lat < 30, lat + 5 + 3, lat),
                       long = ifelse(long < (-10), (long + 3), long))
 
-# Dibujar linea para separar Canarias
+# Dibujar línea para separar Canarias
 lineas_canarias <- data.frame(long = c(-15, -9.5, -9.5),
                               lat =  c(34.5 + 3,  34.5 + 3, 32.5 + 3))
 
 #Cargar tema personalizado 
-# TODO ¿Cambiar el color de fondo?
+
 tema <- function(...) {
         ggplot2::theme_minimal() +
                 ggplot2::theme(
@@ -90,6 +90,7 @@ tema <- function(...) {
                         # panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
                         panel.grid.major = ggplot2::element_blank(),
                         panel.grid.minor = ggplot2::element_blank(),
+                        # TODO ¿Cambiar el color de fondo?
                         plot.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA), 
                         panel.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA), 
                         legend.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
@@ -101,12 +102,14 @@ tema <- function(...) {
 }
 
 # Dibujar gráfica
-# TODO Función todas las variables de interes en bucle
+# TODO Función todas las variables de interés en bucle
+
+# ia14
 ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
         ggplot2::geom_polygon(ggplot2::aes(fill = ia14), 
                               color = "gray50", size = 0.3) +
         
-        # Linea de separación de Canarias
+        # Línea de separación de Canarias
         ggplot2::geom_path(data = lineas_canarias, 
                            ggplot2::aes(long, lat, group = NULL),
                            colour = "gray50", size = 1.3, alpha = 0.5) +
@@ -118,10 +121,34 @@ ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
         tema() +
         
         # Etiquetas 
-        ggplot2::labs(title = "Covid-19 Incidencia Acumulada 14",
+        ggplot2::labs(title = "Covid-19 Incidencia Acumulada 14 días",
                       subtitle = paste("Actualización:", fecha),
                       caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p
 
 ggplot2::ggsave("mapas_provincias/plots/ia14.png", p, 
                 width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
 
+
+# deceassed_per_100000
+ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
+        ggplot2::geom_polygon(ggplot2::aes(fill = deceassed_per_100000), 
+                              color = "gray50", size = 0.3) +
+        
+        # Línea de separación de Canarias
+        ggplot2::geom_path(data = lineas_canarias, 
+                           ggplot2::aes(long, lat, group = NULL),
+                           colour = "gray50", size = 1.3, alpha = 0.5) +
+        
+        # Escala de colores viridis
+        ggplot2::scale_fill_viridis_c(option = "magma", direction = -1) +
+        
+        # Tema personalizado
+        tema() +
+        
+        # Etiquetas 
+        ggplot2::labs(title = "Covid-19 Fallecidos por cada 100 mil hab.",
+                      subtitle = paste("Actualización:", fecha),
+                      caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p2
+
+ggplot2::ggsave("mapas_provincias/plots/fallecidos100mil.png", p2, 
+                width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
