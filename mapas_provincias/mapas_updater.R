02df94d5df097c2c1d2da39a7cc8d7ad7e512719
J. Kniffki <26249992+kifirifis@users.noreply.github.com>
2020-12-08 04:13:37 +0100

    udpate mapas fix ratio

--- mapas_provincias/mapas_updater.R
+++ mapas_provincias/mapas_updater.R
@@ -20,6 +20,10 @@ if (!("raster" %in% installed.packages())) {
 if (!("dplyr" %in% installed.packages())) {
         install.packages("dplyr")
 }
+if (!("patchwork" %in% installed.packages())) {
+        install.packages("patchwork")
+}
+require(patchwork)
 `%>%` <- dplyr::`%>%` # Definición del pipe 
 
 #====== Cartografía y datos ========== 
@@ -48,7 +52,7 @@ df <- read.csv("https://raw.githubusercontent.com/montera34/escovid19data/master
 #======= Preparación de los datos ======== 
 
 # TODO Hacer función bucle con varias variables
-fecha <- Sys.Date() - 4 # 3 días antes que suelen estar completos los datos
+fecha <- Sys.Date() - 6 # 3 días antes que suelen estar completos los datos
 
 # Último día completo y var ia14
 datos <- df %>% # TODO Renombrar variables para hacer el bucle
@@ -67,15 +71,14 @@ ifelse(
 
 #========= "acercar" Canarias 
 cerca <- unido %>% 
-        dplyr::mutate(lat = ifelse(lat < 30, lat + 5 + 3, lat),
-                      long = ifelse(long < (-10), (long + 3), long))
+        dplyr::mutate(lat = ifelse(lat < 30, lat + 7, lat),
+                      long = ifelse(long < (-10), (long + 6.3), long))
 
 # Dibujar línea para separar Canarias
-lineas_canarias <- data.frame(long = c(-15, -9.5, -9.5),
-                              lat =  c(34.5 + 3,  34.5 + 3, 32.5 + 3))
+lineas_canarias <- data.frame(long = c(-14.5 + 2.7, -9.5 + 2.7, -9.5 + 2.7),
+                              lat =  c(34.5 + 2, 34.5 + 2, 32.5 + 2))
 
 #Cargar tema personalizado 
-
 tema <- function(...) {
         ggplot2::theme_minimal() +
                 ggplot2::theme(
@@ -91,9 +94,9 @@ tema <- function(...) {
                         panel.grid.major = ggplot2::element_blank(),
                         panel.grid.minor = ggplot2::element_blank(),
                         # TODO ¿Cambiar el color de fondo?
-                        plot.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA), 
-                        panel.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA), 
-                        legend.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+                        plot.background = ggplot2::element_rect(fill =   "#e6e6e1", color = NA), 
+                        panel.background = ggplot2::element_rect(fill =  "#e6e6e1", color = NA), 
+                        legend.background = ggplot2::element_rect(fill = "#e6e6e1", color = NA),
                         panel.border = ggplot2::element_blank(),
                         legend.title = ggplot2::element_blank(),
                         legend.position = "bottom",
@@ -104,18 +107,19 @@ tema <- function(...) {
 # Dibujar gráfica
 # TODO Función todas las variables de interés en bucle
 
-# ia14
+#===== ia14 =====
 ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
         ggplot2::geom_polygon(ggplot2::aes(fill = ia14), 
                               color = "gray50", size = 0.3) +
         
         # Línea de separación de Canarias
-        ggplot2::geom_path(data = lineas_canarias, 
+        ggplot2::geom_path(data = lineas_canarias,
                            ggplot2::aes(long, lat, group = NULL),
                            colour = "gray50", size = 1.3, alpha = 0.5) +
         
         # Escala de colores viridis
-        ggplot2::scale_fill_viridis_c(option = "magma", direction = -1) +
+        ggplot2::scale_fill_fermenter(palette = "Blues", direction = 1) +
+        ggplot2::coord_equal() +
         
         # Tema personalizado
         tema(legend.key.size = ggplot2::unit(1,"line"),
@@ -124,7 +128,7 @@ ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
         # Etiquetas 
         ggplot2::labs(title = "Covid-19 Incidencia Acumulada 14 días",
                       subtitle = paste("Actualización:", fecha),
-                      caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p
+                      caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") 
 
 ggplot2::ggsave("mapas_provincias/plots/ia14.png", p, 
                 width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
@@ -141,16 +145,68 @@ ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
                            colour = "gray50", size = 1.3, alpha = 0.5) +
         
         # Escala de colores viridis
-        ggplot2::scale_fill_viridis_c(option = "magma", direction = -1) +
+        # ggplot2::scale_fill_viridis_c(option = "magma", direction = -1) +
+        ggplot2::scale_fill_fermenter(direction = 1, palette = "Blues") +
         
         # Tema personalizado
         tema(legend.key.size = ggplot2::unit(1,"line"),
              legend.key.width = ggplot2::unit(2, "cm" )) +
         
         # Etiquetas 
-        ggplot2::labs(title = "Covid-19 Fallecidos por cada 100 mil hab.",
+        ggplot2::labs(title = "Mortalidad COVID-19 (Fallecidos por 100 mil habitantes)",
                       subtitle = paste("Actualización:", fecha),
                       caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p2
 
 ggplot2::ggsave("mapas_provincias/plots/fallecidos100mil.png", p2, 
                 width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
+
+#=============================================================
+#============ Mapa con barras a un lado ======================
+#=============================================================
+
+#Mapa sin leyenda
+ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
+        ggplot2::geom_polygon(ggplot2::aes(fill = deceassed_per_100000), 
+                              color = "gray50", size = 0.3) +
+        # Línea de separación de Canarias
+        ggplot2::geom_path(data = lineas_canarias,
+                           ggplot2::aes(long, lat, group = NULL),
+                           colour = "gray50", size = 1, alpha = 0.5) +
+        # Paleta         
+        ggplot2::scale_fill_fermenter(direction = 1, palette = "Blues") +
+        # aspect.ratio 
+        ggplot2::coord_cartesian(xlim = c(-11.3, 3)) +
+        # Barra más grande
+        tema(legend.key.size = ggplot2::unit(0.9,"line"),
+             legend.key.width = ggplot2::unit(2, "cm" )) -> p3
+
+#barras
+datos %>% 
+        ggplot2::ggplot(ggplot2::aes(reorder(province, deceassed_per_100000), 
+                   deceassed_per_100000, fill = deceassed_per_100000)) +
+        ggplot2::geom_bar(stat = "identity", 
+                          colour = c("#8CAEC2"),
+                          position = "identity") +
+        ggplot2::coord_flip() +
+        ggplot2::scale_fill_fermenter(palette = "Blues", direction = 1) +
+        ggplot2::geom_text(ggplot2::aes(label = deceassed_per_100000), hjust = -0.05, size = 3) +
+        ggplot2::theme(legend.key.size = ggplot2::unit(0.9,"line"),
+                       legend.key.width = ggplot2::unit(2, "cm" ),
+                       legend.position = "bottom",
+                       plot.background = ggplot2::element_rect(fill =   "#e6e6e1", color = NA), 
+                       panel.background = ggplot2::element_rect(fill =  "#e6e6e1", color = NA), 
+                       legend.background = ggplot2::element_rect(fill = "#e6e6e1", color = NA),
+                       legend.title = ggplot2::element_blank()) + 
+        ggplot2::labs(x = "", y = "") -> p4
+
+#Composición 
+p5 <- p3 | p4  
+p5 <- p5 + 
+        plot_annotation(title = "Mortalidad COVID-19 (Fallecidos por 100 mil habitantes)",
+                        subtitle = paste("Actualización: ", fecha, "| ", "Datos: @escovid19data"),
+                        caption = "Geometría: IGN | Elaboración: @JKniffki - KStats®") 
+
+# Guardar 
+ggplot2::ggsave("mapas_provincias/plots/fallecidos.png", p5,
+                width = 68, height = 33, units = "cm")
+
