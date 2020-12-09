756020e0647e4ae034f207db26c0e7bac892342c
J. Kniffki <26249992+kifirifis@users.noreply.github.com>
2020-12-09 16:53:44 +0100

    udpate mapas

--- mapas_provincias/Mapa_segunda_ola.R
+++ mapas_provincias/Mapa_segunda_ola.R
@@ -1,3 +1,4 @@
+                                #Segunda ola 
 #========= Dependencias =======
 if (!("rgdal" %in% installed.packages())) {
         install.packages("rgdal")
@@ -24,6 +25,7 @@ if (!("patchwork" %in% installed.packages())) {
         install.packages("patchwork")
 }
 require(patchwork)
+fecha <- Sys.Date() 
 `%>%` <- dplyr::`%>%` # Definición del pipe 
 
 #====== Cartografía y datos ========== 
@@ -50,19 +52,23 @@ shp_df <- dplyr::left_join(shp_df, shp@data, by = "id")
 df <- read.csv("https://raw.githubusercontent.com/montera34/escovid19data/master/data/output/covid19-provincias-spain_consolidated.csv", header = T, encoding = "UTF-8")  #TODO Probar cargar con fread
 
 #======= Preparación de los datos ======== 
-
-# TODO Hacer función bucle con varias variables
-fecha <- Sys.Date() - 6 # 3 días antes que suelen estar completos los datos
-
-# Último día completo y var ia14
+# La variable deceased son fallecidos acumulados
 datos <- df %>% # TODO Renombrar variables para hacer el bucle
-        dplyr::select(date, province, ine_code, ia14, deceassed_per_100000) %>% 
-        dplyr::filter(date == fecha)
+        dplyr::select(date, province, ine_code, deceased, poblacion) %>% 
+        dplyr::filter(date >= "2020-06-30") %>% #Tomamos un día antes para saber cuantos murieron el día 1 de Julio
+        dplyr::group_by(province, poblacion) %>% 
+        dplyr::mutate(fallecidos = deceased - dplyr::lag(deceased)) %>% 
+        dplyr::filter(date >= "2020-07-01")  %>%  #Quitamos el día antes del primero de Julio
+        dplyr::ungroup() %>% 
+        dplyr::group_by(province, poblacion, ine_code) %>% 
+        dplyr::summarise(suma_fallecidos = sum(fallecidos, na.rm = T)) %>% 
+        dplyr::ungroup() %>% 
+        dplyr::mutate(fallecidos100mil = round((suma_fallecidos / poblacion) * 100000, 2) ) 
+datos
 
 shp_df$ine_code <- as.numeric(shp_df$ine_code) # Numérico como en los datos
 
 #Unión shp_df-datos
-
 ifelse( 
         dim(dplyr::anti_join(datos, shp_df))[1] == 0,
         unido <- dplyr::left_join(shp_df, datos, by = "ine_code"),
@@ -104,70 +110,12 @@ tema <- function(...) {
                 )
 }
 
-# Dibujar gráfica
-# TODO Función todas las variables de interés en bucle
-
-#===== ia14 Incidencia acumulada 14 días =====
-ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
-        ggplot2::geom_polygon(ggplot2::aes(fill = ia14), 
-                              color = "gray50", size = 0.3) +
-        
-        # Línea de separación de Canarias
-        ggplot2::geom_path(data = lineas_canarias,
-                           ggplot2::aes(long, lat, group = NULL),
-                           colour = "gray50", size = 1.3, alpha = 0.5) +
-        
-        # Escala de colores viridis
-        ggplot2::scale_fill_fermenter(palette = "Blues", direction = 1) +
-        ggplot2::coord_equal() +
-        
-        # Tema personalizado
-        tema(legend.key.size = ggplot2::unit(1,"line"),
-             legend.key.width = ggplot2::unit(2, "cm" )) +
-        
-        # Etiquetas 
-        ggplot2::labs(title = "Covid-19 Incidencia Acumulada 14 días",
-                      subtitle = paste("Actualización:", fecha),
-                      caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p
-
-ggplot2::ggsave("mapas_provincias/plots/ia14.png", p, 
-                width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
-
-
-#====== deceassed_per_100000 Esto es sólo el mapa por el momento no se actualiza =======
-# ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
-#         ggplot2::geom_polygon(ggplot2::aes(fill = deceassed_per_100000), 
-#                               color = "gray50", size = 0.3) +
-#         
-#         # Línea de separación de Canarias
-#         ggplot2::geom_path(data = lineas_canarias, 
-#                            ggplot2::aes(long, lat, group = NULL),
-#                            colour = "gray50", size = 1.3, alpha = 0.5) +
-#         
-#         # Escala de colores viridis
-#         # ggplot2::scale_fill_viridis_c(option = "magma", direction = -1) +
-#         ggplot2::scale_fill_fermenter(direction = 1, palette = "Blues") +
-#         
-#         # Tema personalizado
-#         tema(legend.key.size = ggplot2::unit(1,"line"),
-#              legend.key.width = ggplot2::unit(2, "cm" )) +
-#         
-#         # Etiquetas 
-#         ggplot2::labs(title = "Mortalidad COVID-19 (Fallecidos por 100 mil habitantes)",
-#                       subtitle = paste("Actualización:", fecha),
-#                       caption = "Datos: @escovid19data | Geometría: IGN | Elaboración: @JKniffki - KStats®") -> p2
-# 
-# ggplot2::ggsave("mapas_provincias/plots/fallecidos100mil.png", p2, 
-#                 width = 39, height = 26, units = "cm") # TODO Renombrar con bucle 
-
-
 #=============================================================
 #============ Mapa con barras a un lado ======================
 #=============================================================
-
 #Mapa sin leyenda
 ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
-        ggplot2::geom_polygon(ggplot2::aes(fill = deceassed_per_100000), 
+        ggplot2::geom_polygon(ggplot2::aes(fill = fallecidos100mil), 
                               color = "gray50", size = 0.3) +
         # Línea de separación de Canarias
         ggplot2::geom_path(data = lineas_canarias,
@@ -179,40 +127,37 @@ ggplot2::ggplot(cerca, ggplot2::aes(long, lat, group = group)) +
         ggplot2::coord_cartesian(xlim = c(-11.3, 3)) +
         # Barra más grande
         tema(legend.key.size = ggplot2::unit(0.9,"line"),
-             legend.key.width = ggplot2::unit(2, "cm" )) -> p3
-
+             legend.key.width = ggplot2::unit(2, "cm" )) -> p13
 #barras
 datos %>% 
-        ggplot2::ggplot(ggplot2::aes(reorder(province, deceassed_per_100000), 
-                   deceassed_per_100000, fill = deceassed_per_100000)) +
+        ggplot2::ggplot(ggplot2::aes(reorder(province, fallecidos100mil), 
+                                     fallecidos100mil, fill = fallecidos100mil)) +
         ggplot2::geom_bar(stat = "identity", 
                           colour = c("#8CAEC2"),
                           position = "identity") +
         ggplot2::coord_flip() +
         ggplot2::scale_fill_fermenter(palette = "Blues", direction = 1) +
-        ggplot2::geom_text(ggplot2::aes(label = deceassed_per_100000, fontface = 2), 
+        ggplot2::geom_text(ggplot2::aes(label = fallecidos100mil, fontface = 2), 
                            hjust = -0.05, size = 3) +
         ggplot2::theme(
-                       text = ggplot2::element_text(size = 15),
-                       legend.key.size = ggplot2::unit(0.9,"line"),
-                       legend.key.width = ggplot2::unit(2, "cm" ),
-                       legend.position = "bottom",
-                       plot.background = ggplot2::element_rect(fill =   "#e6e6e1", color = NA), 
-                       panel.background = ggplot2::element_rect(fill =  "#e6e6e1", color = NA), 
-                       legend.background = ggplot2::element_rect(fill = "#e6e6e1", color = NA),
-                       legend.title = ggplot2::element_blank()) + 
-        ggplot2::scale_y_continuous(limits = c(0, max(datos$deceassed_per_100000, na.rm = T) + 7),
+                text = ggplot2::element_text(size = 15),
+                legend.key.size = ggplot2::unit(0.9,"line"),
+                legend.key.width = ggplot2::unit(2, "cm" ),
+                legend.position = "bottom",
+                plot.background = ggplot2::element_rect(fill =   "#e6e6e1", color = NA), 
+                panel.background = ggplot2::element_rect(fill =  "#e6e6e1", color = NA), 
+                legend.background = ggplot2::element_rect(fill = "#e6e6e1", color = NA),
+                legend.title = ggplot2::element_blank()) + 
+        ggplot2::scale_y_continuous(limits = c(0, max(datos$fallecidos100mil, na.rm = T) + 7),
                                     expand = c(0.01, 0.01)) +
-        ggplot2::labs(x = "", y = "") -> p4
-
+        ggplot2::labs(x = "", y = "") -> p14
 #Composición 
-p5 <- p3 | p4  
-p5 <- p5 + 
-        plot_annotation(title = "Mortalidad COVID-19 (Ratio fallecidos por cada 100 mil habitantes)",
+p15 <- p13 | p14  
+p15 <- p15 + 
+        plot_annotation(title = "Fallecidos por cada 100 mil habitantes desde el 1 de Julio",
                         subtitle = paste("Actualización: ", fecha, "| ", "Datos: @escovid19data"),
                         caption = "Geometría: IGN | Elaboración: @JKniffki - KStats®") +
         plot_layout(nrow = 1, widths = c(3, 2) ) & ggplot2::theme(title = ggplot2::element_text(size = 25))
-        
 # Guardar 
-ggplot2::ggsave("mapas_provincias/plots/fallecidos.png", p5,
-                width = 68, height = 33, units = "cm")
+ggplot2::ggsave("mapas_provincias/plots/segundaola.png", p15,
+                width = 68, height = 33, units = "cm")                                
